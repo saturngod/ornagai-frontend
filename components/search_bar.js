528daf67f27c73994d8fc63b9558391cd958cb13
saturngod <saturngod@gmail.com>
2022-10-01 01:46:09 +0630

    support the synonymlist

--- components/search_bar.js
+++ components/search_bar.js
@@ -1,56 +1,60 @@
+import { TextField } from '@mui/material';
 import Autocomplete from '@mui/material/Autocomplete';
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react';
 import { createHmac } from "crypto"
 export default function SearchBar({ word }) {
-
+    
     const [wordList, setWordList] = useState([])
     const [value, setValue] = useState(word)
     const router = useRouter()
 
-    const [searchValue, setSearchValue] = useState("")
-
-    useEffect(() => {
-
-        if (searchValue.trim() != "") {
-            const timestamp = Date.now()
-            const app_id = "23321321"
-
-            const key = "69722703123"
-            const original_string = app_id + ":" + timestamp
-            const hash = createHmac('sha1', key)
-                .update(original_string)
-                .digest('hex')
-
-
-            fetch(`http://localhost:3000/api/search/${searchValue}?timestamp=${timestamp}&hash=${hash}`, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'app_id': app_id
-                },
-            }).then((response) => {
-                //setWordList(response.result)
-                response.json().then(value => setWordList(value.result))
-            })
-        }
-    })
+    
 
     return (
         <Autocomplete
             id="Dictionary"
 
             onChange={(event, newValue) => {
-                router.push("/result/" + newValue)
+                console.log(newValue)
+                if(newValue != undefined) {
+                    router.push("/result/" + encodeURIComponent(newValue))
+                }
             }
             }
             value={value}
             onInputChange={(event, newValue) => {
-                setSearchValue(newValue)
+
+                setValue(newValue)
+
+                if (newValue.trim() != "") {
+                    const timestamp = Date.now()
+                    const app_id = "23321321"
+        
+                    const key = "69722703123"
+                    const original_string = app_id + ":" + timestamp
+                    const hash = createHmac('sha1', key)
+                        .update(original_string)
+                        .digest('hex')
+        
+        
+                    fetch(`http://localhost:3000/api/search/${newValue}?timestamp=${timestamp}&hash=${hash}`, {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                            'app_id': app_id
+                        },
+                    }).then((response) => {
+                        //setWordList(response.result)
+                        response.json().then(value => setWordList(value.result))
+                    })
+                }
+
+                
             }}
             options={wordList}
-
-            renderInput={(params) => <TextField {...params} label="Dictionary" />}
+            
+            renderInput={(params) => <TextField {...params} label="Enter text to search" />}
         />
     )
 }
--- pages/result/[word].js
+++ pages/result/[word].js
@@ -1,10 +1,17 @@
 import { createHmac } from "crypto"
 import Container from '@mui/material/Container';
+import { useRouter } from 'next/router'
 import Stack from '@mui/material/Stack';
 import SearchBar from '../../components/search_bar'
+import Grid from "@mui/material/Unstable_Grid2/Grid2";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 
 export default function Result({ result }) {
+
+
     const res = result.result
+
+
     if (res == null) {
         return (
             <div>Not Found</div>
@@ -19,24 +26,78 @@ export default function Result({ result }) {
     var ornagai = res.ornagai
     var ornagai_text = ""
     if (ornagai != null) {
-        ornagai_text = ornagai.map((value) => "<div class='ornagai'><div class='state'>" + value.state + "</div><div class='def'>" + value.def + "</div></div>").join()
+        ornagai_text = ornagai.map((value) => {
+            if (value.def == "") {
+                return ""
+            }
+            return `<div class='ornagai'>
+             <div class='state'>${value.state}</div>
+             <div class='def'>${value.def}</div>
+             </div>
+             `
+
+        }).join("")
     }
 
     var engmm = res.eng_mm
     var engmm_text = ""
+    var synonym = ""
+    var synonymlist = []
     if (engmm != null) {
         engmm_text = engmm.map((value) => "<div class='engmm'><div class='def'>" + value.definition + "</div></div>").join()
+
+        synonym = engmm.map((value) => {
+            return value.synonym
+        }).join(",")
+        if(synonym != "," && synonym != "") {
+            synonymlist = synonym.split(",")
+        }
+        
     }
 
+    var spacing = 2
+    var right = 10
+    if (synonym == "") {
+        spacing = 0
+        right = 12
+    }
+
+
+    const router = useRouter()
+    console.log(synonymlist)
     return (
-        <Container>
-            
+        <Container sx={{ pt: 5 }}>
+
             <Stack>
-            <SearchBar word={res.word}></SearchBar>
+                <SearchBar word={res.word}></SearchBar>
                 <h1>{res.word}</h1>
-                <div dangerouslySetInnerHTML={{ __html: ornagai_text }}></div>
-                <div dangerouslySetInnerHTML={{ __html: engmm_text }}></div>
-                <div dangerouslySetInnerHTML={{ __html: oxford_text }}></div>
+                <Grid container spacing={2}>
+                    <Grid xs={spacing}>
+                        {
+                            synonymlist.length > 0 &&
+                            <span className='synonym'>synonym</span>
+                        }
+                        <nav>
+                            <List>
+                                {
+                                    synonymlist.map((val) => {
+                                        return (<ListItem key={val} disablePadding>
+                                            <ListItemButton onClick={(event) => router.push("/result/" + encodeURIComponent(val))}>
+                                                <ListItemText primary={val}></ListItemText>
+                                            </ListItemButton>
+                                        </ListItem>)
+                                    })
+                                }
+
+                            </List>
+                        </nav>
+                    </Grid>
+                    <Grid xs={right}>
+                        <div className="ornagai_data" dangerouslySetInnerHTML={{ __html: ornagai_text }}></div>
+                        <div className="engmm_data" dangerouslySetInnerHTML={{ __html: engmm_text }}></div>
+                        <div className="oxford_data" dangerouslySetInnerHTML={{ __html: oxford_text }}></div>
+                    </Grid>
+                </Grid>
             </Stack>
         </Container>
 
@@ -44,7 +105,8 @@ export default function Result({ result }) {
 }
 
 export async function getServerSideProps(context) {
-    const word = context.query.word
+    const word = encodeURIComponent(context.query.word)
+
 
     const timestamp = Date.now()
     const app_id = "23321321"

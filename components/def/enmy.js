6f896f0288720390b5ccd564727af1fdfc237ed8
saturngod <saturngod@gmail.com>
2022-10-02 17:06:25 +0630

    support image

--- components/def/enmy.js
+++ components/def/enmy.js
@@ -9,6 +9,13 @@ export default function EnMyText({ data }) {
                 data.map((val, index) => {
                     return (
                         <div className="engmm" key={"engmm_" + index}>
+                            <div className="pic">
+                                {
+                                    val.picture != null &&
+                                    <img className="img-example" src={"/images/" + val.picture} />
+                                }
+
+                            </div>
                             <div className="def">
                                 <div className="engmm_data" dangerouslySetInnerHTML={{ __html: val.definition }}></div>
                             </div>

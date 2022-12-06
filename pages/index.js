b985adeb34123e88a126fddc7782ab75ef1468f0
saturngod <saturngod@gmail.com>
2022-12-06 15:13:05 +0630

    add ads

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,38 @@
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import SearchBar from '../components/search_bar';
+import Script from 'next/script'
 
 
 
 
-export default function Home({word}) {
+export default function Home({ word }) {
 
-  
 
-  
-  
+
+
+
 
   return (
     <Container sx={{ pt: 1 }}>
-<h1>Ornagai</h1>
+      <h1>Ornagai</h1>
       <Stack spacing={2}>
-      <SearchBar word=""></SearchBar>
-        
-
-       
+        <SearchBar word=""></SearchBar>
       </Stack>
-
+      <Container sx={{ pt: 1 }}>
+        <Script>
+          {`
+	atOptions = {
+		'key' : '7e4f681717ace319e62de052b7970e7d',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/7e4f681717ace319e62de052b7970e7d/invoke.js"></scr' + 'ipt>');
+  `}
+        </Script>
+      </Container>
     </Container>
   )
 }
